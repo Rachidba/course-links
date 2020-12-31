69cf939a4d9b62726be827642879af8032edb661
Rachid BAAZIZ <rachidbaaziz.contact@gmail.com>
2020-12-31 01:29:36 +0100

    Configure a mock api for tests
    Create mock api with json-server

---
+++ tools/mockData.js
@@ -0,0 +1,76 @@
+const curriculums = [
+  {
+    title: "Introduction to web developement",
+    slug: "introduction-to-web-developement",
+    description: "A list of ressources to help you understand Web developement",
+    link: "",
+    items: [
+      {
+          title: "A Complete Web Development Learning Path",
+          Description: "Web Development is complex, there are so many technologies you might've heard of. Here's a simple and clear path on how to become a web developer today!",
+          link: "https://www.youtube.com/watch?v=2-akTTZyrcM&ab_channel=Academind",
+          type: "ENUM_YOUTUBE"
+      },
+      {
+        title: "How The Web Works - The Big Picture",
+        Description: "From request to response - how does the web work? Let's have a look at a high-level overview.",
+        link: "https://www.youtube.com/watch?v=hJHvdBlSxug&ab_channel=Academind",
+        type: "ENUM_YOUTUBE"
+      },
+      {
+        title: "Dynamic Websites vs Static Pages vs Single Page Apps (SPAs)",
+        Description: "Should you only build SPAs? Why might dynamic pages be better? And there even is a third option!",
+        link: "https://www.youtube.com/watch?v=Kg0Q_YaQ3Gk&ab_channel=Academind",
+        type: "ENUM_YOUTUBE"
+      }
+    ]
+  },
+  {
+    title: "Lean JavaScript DOM",
+    slug: "lean-javascript-dom",
+    description: "A list of ressources to help you understand JavaScript DOM",
+    link: "I'm sharing with you this playlist of vidios made by 3arbaoui Mehdi",
+    items: [
+      {
+          title: "What's DOM (Document Object Model) & DOM VS BOM (Browser Object Model)",
+          Description: "",
+          link: "https://www.youtube.com/watch?v=vXqe3BfiK90&list=PLpXCAa5_ykleN-CRRLDBwL-eBYWqdsgpc&ab_channel=3arbaouiMehdi",
+          type: "ENUM_YOUTUBE"
+      },
+      {
+        title: "How to use event listeners to access a DOM Element (DOMContentLoaded)",
+        Description: "",
+        link: "https://www.youtube.com/watch?v=u35dNoeTp5M&list=PLpXCAa5_ykleN-CRRLDBwL-eBYWqdsgpc&index=2&ab_channel=3arbaouiMehdi",
+        type: "ENUM_YOUTUBE"
+      },
+      {
+        title: "HTML Script Tag (Inline vs. Async vs. Defer)",
+        Description: "",
+        link: "https://www.youtube.com/watch?v=pVG_VZ0R_QU&list=PLpXCAa5_ykleN-CRRLDBwL-eBYWqdsgpc&index=3&ab_channel=3arbaouiMehdi",
+        type: "ENUM_YOUTUBE"
+      },
+      {
+        title: "DOM Nodes Demystified",
+        Description: "",
+        link: "https://www.youtube.com/watch?v=uyQueE7Pbww&list=PLpXCAa5_ykleN-CRRLDBwL-eBYWqdsgpc&index=4&ab_channel=3arbaouiMehdi",
+        type: "ENUM_YOUTUBE"
+      },
+      {
+        title: "Add DOM Nodes into the DOM Tree",
+        Description: "",
+        link: "https://www.youtube.com/watch?v=9syRy35XqmU&list=PLpXCAa5_ykleN-CRRLDBwL-eBYWqdsgpc&index=5&ab_channel=3arbaouiMehdi",
+        type: "ENUM_YOUTUBE"
+      },
+      {
+        title: "Get and Set HTML Content by using innerHTML & Security Manipulation",
+        Description: "",
+        link: "https://www.youtube.com/watch?v=rwq2VvFFOfU&list=PLpXCAa5_ykleN-CRRLDBwL-eBYWqdsgpc&index=6&ab_channel=3arbaouiMehdi",
+        type: "ENUM_YOUTUBE"
+      },
+    ]
+  },
+];
+
+module.exports = {
+  curriculums
+};
